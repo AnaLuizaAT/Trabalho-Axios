f3f649b76cbaf9a4771362e57136e4ece15a1019
Ana Luiza Araujo <aluiza.araujot@gmail.com>
2023-08-28 22:46:21 -0300

    fix

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import { get } from 'axios';
+const axios = require('axios');
 
 async function fetchPost() {
   const apiUrl = 'https://jsonplaceholder.typicode.com/posts/1';
 
   try {
-    const response = await get(apiUrl);
+    const response = await axios.get(apiUrl);
     console.log('Resposta:', response.data);
   } catch (error) {
     console.error('Ocorreu um erro:', error.message);
